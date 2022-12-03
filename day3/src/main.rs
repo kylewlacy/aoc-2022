52222526f409f7fe535c81c74d8a8f7736abd9f0
Kyle Lacy <kylelacy@kyle.space>
2022-12-03 00:37:40 -0800

    Solve day 3 part 2

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,20 +1,23 @@
+#![feature(iter_array_chunks)]
+
 use std::{collections::BTreeSet, io::BufRead};
 
 fn main() -> anyhow::Result<()> {
     let stdin = std::io::stdin().lock();
 
-    let mut common_items: Vec<char> = vec![];
-    for line in stdin.lines() {
-        let line = line?;
-        let compartment_size = line.len() / 2;
-        let (first_compartment, second_compartment) = line.split_at(compartment_size);
-        let first_compartment: BTreeSet<char> = first_compartment.chars().collect();
-        let second_compartment: BTreeSet<char> = second_compartment.chars().collect();
+    let mut badges: Vec<char> = vec![];
+    for [a, b, c] in stdin.lines().array_chunks() {
+        let [a, b, c] = [a?, b?, c?];
+        let a: BTreeSet<char> = a.chars().collect();
+        let b: BTreeSet<char> = b.chars().collect();
+        let c: BTreeSet<char> = c.chars().collect();
 
-        common_items.extend(first_compartment.intersection(&second_compartment));
+        let ab: BTreeSet<char> = a.intersection(&b).copied().collect();
+        let abc = ab.intersection(&c);
+        badges.extend(abc);
     }
 
-    let total_priority: u64 = common_items
+    let total_priority: u64 = badges
         .iter()
         .map(|&item| -> u64 { priority(item).into() })
         .sum();
