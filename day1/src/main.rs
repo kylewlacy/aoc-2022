468315bdd22662ee4fdab602e9889dc55f6a6c21
Kyle Lacy <kylelacy@kyle.space>
2022-12-02 20:40:58 -0800

    Solve day 1 part 2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,30 +1,63 @@
+use clap::Parser;
 use std::io::BufRead;
 
+#[derive(Debug, Default)]
+struct Elves {
+    top_slots: usize,
+    top_elves: Vec<u64>,
+    current_elf: u64,
+}
+
+impl Elves {
+    fn new(top_slots: usize) -> Self {
+        Elves {
+            top_slots,
+            top_elves: Vec::with_capacity(top_slots + 1),
+            current_elf: 0,
+        }
+    }
+
+    fn add_current(&mut self, calories: u64) {
+        self.current_elf += calories;
+    }
+
+    fn end_current(&mut self) -> &[u64] {
+        let current = std::mem::replace(&mut self.current_elf, 0);
+        self.top_elves.push(current);
+        self.top_elves.sort_by_key(|&elf| std::cmp::Reverse(elf));
+        self.top_elves.truncate(self.top_slots);
+
+        &self.top_elves
+    }
+}
+
+#[derive(Debug, Parser)]
+struct Args {
+    #[arg(long)]
+   top_slots: usize,
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = Args::parse();
+
     let stdin = std::io::stdin().lock();
 
-    let mut current_most = 0;
-    let mut best_most = 0;
+    let mut elves = Elves::new(args.top_slots);
     for line in stdin.lines() {
         let line = line?;
 
         if line.is_empty() {
-            if current_most > best_most {
-                best_most = current_most;
-            }
-
-            current_most = 0;
+            elves.end_current();
         } else {
             let calories: u64 = line.parse()?;
-            current_most += calories;
+            elves.add_current(calories);
         }
     }
 
-    if current_most > best_most {
-        best_most = current_most;
-    }
+    let top_elves = elves.end_current();
 
-    println!("{}", best_most);
+    let top_sum: u64 = top_elves.iter().sum();
+    println!("{}", top_sum);
 
     Ok(())
 }
