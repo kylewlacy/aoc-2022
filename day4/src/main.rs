10017d87038dc2a85f4793170dd52d175f8bd339
Kyle Lacy <kylelacy@kyle.space>
2022-12-03 23:32:34 -0800

    Solve day 4 part 2

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -5,7 +5,7 @@ use anyhow::Context;
 fn main() -> anyhow::Result<()> {
     let stdin = std::io::stdin().lock();
 
-    let mut complete_overlaps = 0;
+    let mut partial_overlaps = 0;
     for line in stdin.lines() {
         let line = line?;
         let (first, second) = line.split_once(',').context("could not split pair")?;
@@ -17,17 +17,17 @@ fn main() -> anyhow::Result<()> {
             .context("could not split second range")?;
         let first = first_a.parse::<u64>()?..=first_b.parse::<u64>()?;
         let second = second_a.parse::<u64>()?..=second_b.parse::<u64>()?;
-        if complete_overlap(first, second) {
-            complete_overlaps += 1;
+        if partial_overlap(&first, &second) {
+            partial_overlaps += 1;
         }
     }
 
-    println!("{complete_overlaps}");
+    println!("{partial_overlaps}");
 
     Ok(())
 }
 
-fn complete_overlap(first: RangeInclusive<u64>, second: RangeInclusive<u64>) -> bool {
+fn complete_overlap(first: &RangeInclusive<u64>, second: &RangeInclusive<u64>) -> bool {
     // |--------------|
     //     |-----|
     // fs             fe
@@ -35,4 +35,10 @@ fn complete_overlap(first: RangeInclusive<u64>, second: RangeInclusive<u64>) -> 
     // fs <= ss && fe >= se
     (first.start() >= second.start() && first.end() <= second.end())
         || (second.start() >= first.start() && second.end() <= first.end())
+}
+
+fn partial_overlap(first: &RangeInclusive<u64>, second: &RangeInclusive<u64>) -> bool {
+    complete_overlap(first, second)
+        || ((first.contains(second.start()) || first.contains(second.end()))
+            && (second.contains(first.start()) || second.contains(second.end())))
 }
