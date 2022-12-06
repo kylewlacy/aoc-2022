8aa091fca5c9c254441a358eb70f62d26dd3cf5a
Kyle Lacy <kylelacy@kyle.space>
2022-12-05 23:45:42 -0800

    Solve day 6 part 1

---
+++ day6/src/main.rs
@@ -0,0 +1,31 @@
+use anyhow::Context;
+use itertools::Itertools;
+
+fn main() -> anyhow::Result<()> {
+    let datastream = std::io::stdin()
+        .lines()
+        .next()
+        .context("no input provided")?
+        .context("failed to read input")?;
+
+    let sync_index =
+        datastream
+            .as_bytes()
+            .windows(4)
+            .enumerate()
+            .find_map(|(start_index, bytes)| {
+                for (a, b) in bytes.iter().tuple_combinations() {
+                    if a == b {
+                        return None;
+                    }
+                }
+
+                Some(start_index + bytes.len())
+            });
+
+    let sync_index = sync_index.context("could not sync datastream")?;
+
+    println!("{sync_index}");
+
+    Ok(())
+}
